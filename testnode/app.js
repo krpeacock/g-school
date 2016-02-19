5aeeca6754bf9248da49728cbee3eefbd7d23519
krpeacock <kylpeacock@gmail.com>
2016-02-19 09:11:18 -0800

    set to send to site

--- testnode/app.js
+++ testnode/app.js
@@ -1,15 +1,22 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveStatic = require('serve-static')
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join('public/ftp', 'public')));
+
 
 // Serve up public/ftp folder
-var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
+var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']});
 
 // Create server
 var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-  serve(req, res, done)
+  var done = finalhandler(req, res);
+  serve(req, res, done);
 })
 
 // Listen
-server.listen(8000)
+server.listen(8000);
+console.log("Listening on localhost:8000");
